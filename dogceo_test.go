ae66e328df6eff241f85fc9378cdd07618e6cf5c
Christian Harrypersad <psp1001k2006@gmail.com>
2017-07-24 13:54:13 -0400

    Added test file

---
+++ dogceo_test.go
@@ -0,0 +1,33 @@
+package godogceo
+
+import "testing"
+
+func TestBreedList(t *testing.T) {
+	breedList, err := GetDogBreeds()
+	if err != nil {
+		t.Error(err)
+	}
+	if breedList.Message == nil {
+		t.Error("Recieved an empty map of breeds")
+	}
+}
+
+func TestRandomImage(t *testing.T) {
+	image, err := GetRandomImage()
+	if err != nil {
+		t.Error(err)
+	}
+	if image == "" {
+		t.Error("Received an empty image string")
+	}
+}
+
+func TestRandomBreedImage(t *testing.T) {
+	image, err := GetRandomBreedImage("shiba")
+	if err != nil {
+		t.Error(err)
+	}
+	if image == "" {
+		t.Error("Recieved an empty image string")
+	}
+}
